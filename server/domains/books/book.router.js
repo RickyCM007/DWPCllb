2d2d1a7b2eb276bdcc801c5953cd438981d07286
RickyCM007 <143634880+RickyCM007@users.noreply.github.com>
2023-12-05 16:08:02 +0000

    ❗Validation With Yup

--- server/domains/books/book.router.js
+++ server/domains/books/book.router.js
@@ -2,26 +2,32 @@
 import { Router } from 'express';
 
 // Importando el controlador
-import userController from './book.controller';
+import bookController from './project.controller';
 
-// Creando una instancia del enrutador
+// Importando factory de validación
+import ValidateFactory from '../../services/validateFactory';
+// Importando el validador de proyectos
+import bookValidator from './project.validator';
+
+// Creando una isntancia del enrutador
 const router = new Router();
 
 // Enrutamos
-// GET '/book/book
-router.get('/', userController.dashboard);
-
-// GET '/book/dashboard
-router.get('/dashboard', userController.dashboard);
-
-// GET '/project/add-form
-router.get('book/addbook', userController.addform);
+// GET "/project"
+router.get('/', bookController.showDashboard);
 
 // GET '/project/add
 router.get('/add', bookController.add);
 
 // POST "/project/add"
-router.post('/add', bookController.addPost);
+router.post(
+  '/add',
+  ValidateFactory({
+    schema: bookValidator.projectSchema,
+    getObject: bookValidator.getProject,
+  }),
+  bookController.addPost,
+);
 
 // Exporto este tramo de ruta
 export default router;
