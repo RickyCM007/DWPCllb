031717b5bd0fe07297abcf03787e33127ef40bef
RickyCM007 <143634880+RickyCM007@users.noreply.github.com>
2023-12-06 14:45:40 +0000

    ✅Bug Fixed Port Dev

--- server/services/debugLogger.js
+++ server/services/debugLogger.js
@@ -1,6 +1,6 @@
 // Importando dependencia debug
 import Debug from 'debug';
 // Establecemos el proceso a debuggear
-const debug = Debug('s04e01-proyecto-final:server');
+const debug = Debug('dwpcllb:server');
 // exportando el logger de debuggeo
 export default debug;
